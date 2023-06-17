96a70d8faef026c60d05792ed3de397484f7c543
dejauls <rajauls178@gmail.com>
2023-06-17 12:55:45 +0700

    p

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 app.config ['TEMPLATES_AUTO_RELOAD'] = True
 app.config ['UPLOAD_FOLDER'] = './static/profile_pics'
 
-
 MONGODB_CONNECTION_STRING = 'mongodb://jaul:kelompok4@ac-tckcnrv-shard-00-00.iyckasz.mongodb.net:27017,ac-tckcnrv-shard-00-01.iyckasz.mongodb.net:27017,ac-tckcnrv-shard-00-02.iyckasz.mongodb.net:27017/?ssl=true&replicaSet=atlas-ehh9a0-shard-0&authSource=admin&retryWrites=true&w=majority'
 client = MongoClient(MONGODB_CONNECTION_STRING)
 db = client.umkm
@@ -22,6 +21,11 @@ SECRET_KEY = 'SAYA'
 
 @app.route('/',methods=['GET'])
 def home():
+    return render_template('home.html')
+    
+    
+@app.route('/login',methods=['GET'])
+def login():
     token_receive = request.cookies.get(TOKEN_KEY)
     try : 
         payload = jwt.decode(
@@ -36,7 +40,7 @@ def home():
         return redirect(url_for('login', msg=msg))
     except jwt.exceptions.DecodeError:
         msg = 'There was a promblem logging you in'
-        return redirect(url_for('login', msg=msg))
+        return redirect(url_for('login', msg=msg))   
 
 @app.route('/catalog')
 def catalog():
@@ -56,18 +60,34 @@ def login_admin():
     else:
         return render_template('login_admin.html', error_message='Email atau password salah!')
 
-@app.route('/admin/catalog')
-def admin_catalog():
-    # Logika untuk halaman katalog admin
-    return render_template('admin_cat.html')
 
 @app.route('/admin')
 def admin():
     return render_template('login_admin.html')
 
-@app.route("/login")
-def login():
-    return render_template('login_user.html')
+@app.route('/admin/catalog')
+def admin_catalog():
+    return render_template('admin_cat.html')    
+
+
+@app.route('/admin/daftar_user')
+def user():
+    collection = db.users
+    users = collection.find({})
+   
+    return render_template('daftar_user.html',
+                            users=users
+                            )
+
+@app.route('/admin/list_order')
+def orderan():
+    return render_template('transaksi_admin.html')
+
+
+@app.route('/inputkatalog')
+def inputkatalog():
+    return render_template('inputkatalog.html')
+
 
 @app.route("/sign_in", methods=["POST"])
 def sign_in():
@@ -120,6 +140,76 @@ def signup_save():
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
+
+
+
+# untuk page input katalog
+            #def insert_katalog(jenis, ukuran, harga, deskripsi):
+                #db = connect_mongodb()
+                #collection = db['katalog'] 
+                #katalog = {
+                    #'jenis': jenis,
+                    #'ukuran': ukuran,
+                    #'harga': harga,
+                    #'deskripsi': deskripsi
+                #}
+                #collection.insert_one(katalog)
+
+            # Route untuk halaman edit katalog
+            #@app.route('/editkatalog', methods=['GET', 'POST'])
+            #def edit_katalog():
+                #if request.method == 'POST':
+                    #jenis = request.form.get('jenis')
+                    #ukuran = request.form.get('ukuran')
+                    #harga = request.form.get('harga')
+                    #deskripsi = request.form.get('deskripsi')
+                    #insert_katalog(jenis, ukuran, harga, deskripsi)
+                   #return 'Data berhasil disimpan ke MongoDB!'
+                #return render_template('editkatalog.html')
+
+#untuk pages edit katalog
+        #def get_katalog():
+            #db = connect_mongodb()
+            #collection = db['katalog']  
+            #katalog = collection.find()
+            #return katalog
+
+        # Fungsi untuk mengupdate data katalog 
+        #def update_katalog(jenis, ukuran, harga, deskripsi):
+            #db = connect_mongodb()
+            #collection = db['katalog']  
+            #collection.update_one(
+                #{'jenis': jenis},
+                #{'$set': {'ukuran': ukuran, 'harga': harga, 'deskripsi': deskripsi}}
+            #)
+
+        # Route untuk halaman edit katalog
+        #@app.route('/editkatalog', methods=['GET', 'POST'])
+        #def edit_katalog():
+            #if request.method == 'POST':
+                #jenis = request.form.get('jenis')
+                #ukuran = request.form.get('ukuran')
+                #harga = request.form.get('harga')
+                #deskripsi = request.form.get('deskripsi')
+                #update_katalog(jenis, ukuran, harga, deskripsi)
+                #return 'Data berhasil diupdate di MongoDB!'
+            #else:
+                #katalog = get_katalog()
+                #return render_template('editkatalog.html', katalog=katalog)
+
+
+#untuk page transaksi admin
+        #def get_orders():
+            #client = MongoClient('mongodb://localhost:27017/')  # Ganti URL MongoDB sesuai konfigurasi Anda
+            #db = client['order_database']  # Ganti nama database sesuai dengan nama database Anda
+            #collection = db['orders']  # Ganti nama koleksi sesuai dengan nama koleksi Anda
+            #orders = collection.find()
+            #return orders
+
+        #@app.route('/')
+        #def home():
+            #orders = get_orders()
+            #return render_template('index.html', orders=orders)
 
 
 if __name__ == '__main__':
